aa23a9c92211c30caf5105541e67437b335e6709
William <willdev2025@outlook.com>
2026-02-28 10:16:59 -0800

    moved key storage folder since it was conflicting with router

--- src/keyshare/ks.jsx
+++ src/keyshare/ks.jsx
@@ -7,16 +7,16 @@ export default function Keyshare() {
             <h1>Public GPG/PGP keys</h1>
             <h4>All keys are ED25519/CV25519</h4>
             <div className="card-container">
-                <RedirectButton location="/keys/data_pubkey.bin.asc">
+                <RedirectButton location="/keyfiles/data_pubkey.bin.asc">
                     Binary pubkey (encrypting data)
                 </RedirectButton>
-                <RedirectButton location="/keys/data_pubkey.txt.asc">
+                <RedirectButton location="/keyfiles/data_pubkey.txt.asc">
                     Plaintext pubkey (encrypting data)
                 </RedirectButton>
-                <RedirectButton location="/keys/dev_pubkey.bin.asc">
+                <RedirectButton location="/keyfiles/dev_pubkey.bin.asc">
                     Binary pubkey (signing commits)
                 </RedirectButton>
-                <RedirectButton location="/keys/dev_pubkey.txt.asc">
+                <RedirectButton location="/keyfiles/dev_pubkey.txt.asc">
                     Plaintext pubkey (signing commits)
                 </RedirectButton>
             </div>
